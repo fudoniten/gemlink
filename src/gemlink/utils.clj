b5518ed45cc29a0d0fd6bcdbed24811a4062e9e9
niten <niten@fudo.org>
2025-07-31 13:59:30 -0700

    feat: add gemlink utility functions in utils.clj

---
+++ src/gemlink/utils.clj
@@ -0,0 +1,23 @@
+(ns gemlink.utils)
+
+(defmacro cond-let
+  [& clauses]
+  (when (odd? (count clauses))
+    (throw (IllegalArgumentException. "cond-let requires an even number of forms")))
+  (letfn [(expand [[test then & more]]
+            (cond
+              (and (= test :else) (empty? more)) then
+
+              (vector? test)
+              `(let [temp# ~(second test)]
+                 (if temp#
+                   (let [~(first test) temp#]
+                     ~then)
+                   ~(if (seq more)
+                      (expand more)
+                      (throw (IllegalArgumentException.
+                              "failed to bind in bind-let")))))
+
+              :else (throw (IllegalArgumentException.
+                            (str "cond-let expected a binding vector, got " test)))))]
+    (expand clauses)))
